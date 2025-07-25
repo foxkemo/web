f95c26f6b1500b8187548b439e8dcdaa2b90142b
unomi <taokunkun1@icloud.com>
2025-07-25 20:40:57 +0800

    auto-commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let id="8.133.206.215";
+let loginform=document.getElementById("loginform");
+loginform.action=id+"/login";
+let registerform=document.getElementById("registerform");
+registerform.action=id+"/register";
+let a=document.getElementById("test");
+a.textContent="123";
+alert("1234");
